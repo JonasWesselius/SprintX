8d35f399ecd0eafa4265c69e7a551266c5896a9c
JonasWesselius <112404721+JonasWesselius@users.noreply.github.com>
2025-01-07 18:07:30 +0100

    Font changes

--- sprintx/src/app/layout.tsx
+++ sprintx/src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -38,10 +34,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header /> {/* Header is included here */}
+      <body className={`${roboto.className} antialiased`}>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
